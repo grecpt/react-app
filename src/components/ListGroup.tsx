45a60943b02288cc89a3003f2cbc038628887bd1
grecpt <grecpt@gmail.com>
2024-03-22 13:20:40 +1000

    Update App.tsx with the correct bootstrap css library

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,12 @@
 function ListGroup() {
   return (
-    <ol className="list-group list-group-numbered">
-      <li className="list-group-item">A list item</li>
-      <li className="list-group-item">A list item</li>
-      <li className="list-group-item">A list item</li>
-    </ol>
+    <ul className="list-group">
+      <li className="list-group-item">An item</li>
+      <li className="list-group-item">A second item</li>
+      <li className="list-group-item">A third item</li>
+      <li className="list-group-item">A fourth item</li>
+      <li className="list-group-item">And a fifth one</li>
+    </ul>
   );
 }
 
